15595d86796b36a02c2b6f36abf1cc8d6cfb9353
Rajamani <vrajamani@hidglobal.com>
2017-01-25 12:52:03 +0530

    Hello World Changes

---
+++ HelloW.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author vrajamani
+ *
+ */
+public class HelloW {
+
+	/**
+	 * 
+	 */
+	public HelloW() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcome");
+		System.out.println("Hello World ...");
+
+	}
+
+}
